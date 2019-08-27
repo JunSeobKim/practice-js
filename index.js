98af79e30c1ec990cd28ff007388a98bf025cc07
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-08-27 13:05:16 +0900

    practice

---
+++ index.js
@@ -0,0 +1,24 @@
+// String
+const what = "JunSeob";
+// Boolean
+const b = true;
+const c = false;
+//Number
+const wat = 1234567890;
+//float
+const a = 26.613;
+//Array
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek);
+
+console.log(daysOfWeek[3]);
+//Object
+const junseobInfo = {
+  name: "JunSeob",
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "Darknight"],
+  pet: [{ name: "mozzi", fatty: false }, { name: "neo", fatty: true }]
+};
+
+console.log(junseobInfo.pet[1].name);
