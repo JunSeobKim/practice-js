0ccd72d84c54c5059691c2ee92ee8741fa4fd55e
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-08-30 11:58:18 +0900

    finish

--- todo.js
+++ todo.js
@@ -26,11 +26,11 @@ function paintTodo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerText = "ⓧ";
+  delBtn.innerText = "Ⅹ";
   delBtn.addEventListener("click", deleteToDO);
   span.innerText = text;
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
